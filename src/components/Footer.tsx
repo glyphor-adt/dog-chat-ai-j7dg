115a5e082322756f95a4a4214d63d84cd31db464
glyphor-adt <Kristina@Glyphorai.onmicrosoft.com>
2025-11-24 20:29:22 -0600

    Add src/components/Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className="bg-gray-100 py-12">
+      <div className="container mx-auto text-center">
+        <p className="text-gray-500">Copyright © 2024 BarkBot. All rights reserved.</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
